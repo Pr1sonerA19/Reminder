649362d6226cde0c748b258efb5df7144498145b
Pr1sonerA19 <114249353+Pr1sonerA19@users.noreply.github.com>
2025-09-16 11:47:34 -0600

    Delete Checker.js

--- Checker.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('inputForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Submit');
-  });
-});
