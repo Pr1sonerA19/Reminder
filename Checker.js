8fde821115fc476e215fdab8f0f51ef0ed9db325
Pr1sonerA19 <114249353+Pr1sonerA19@users.noreply.github.com>
2025-09-16 11:35:30 -0600

    Update Checker.js

--- Checker.js
+++ Checker.js
@@ -1,3 +1,6 @@
-document.addEventListener('DOMContentLoaded',function() {
-  alert('Hello');
-});
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('inputForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Submit');
+  });
+);
